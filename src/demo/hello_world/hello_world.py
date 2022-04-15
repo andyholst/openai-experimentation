59283a6dc86e2e13fae8d0f97b595193340fde60
Andy Holst <andy.holst85@gmail.com>
2022-04-15 23:49:26 +0200

    Updated CI configuration (#3)
    Added Docker-Compose files validation CI stage.
    
    Added hello world unit test with Docker-Compose setup.
    
    Added update unit tests Python requirements CI stage.
    
    Added run Python unit tests CI stage.
    
    Added Dockerfile for demo Python module.
    
    Added generic Docker-Compose builder for building specific
    Dockerfile application.
    
    Added build Dockerfile Python demo application CI stage.
    
    Added a run-app Makefile command to run a specific Dockerfile app.
    
    Added a run Docker Python demo CI stage.

---
+++ src/demo/hello_world/hello_world.py
@@ -0,0 +1,10 @@
+def main():
+    return "Hello world!"
+
+
+def print_main():
+    print(main())
+
+
+if __name__ == "__main__":
+    print_main()
---
+++ tests/unit-tests/tests/demo/hello_world/hello_world_test.py
@@ -0,0 +1,9 @@
+from demo.hello_world.hello_world import main, print_main
+
+
+def test_main():
+    assert main() == 'Hello world!'
+
+
+def test_name():
+    assert print_main() is None
