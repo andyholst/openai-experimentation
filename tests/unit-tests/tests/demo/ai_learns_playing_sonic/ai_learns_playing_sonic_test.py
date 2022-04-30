97a488dafd7fc9c75371a424bd21d37deced7eec
Andy Holst <andy.holst@protonmail.com>
2022-04-30 21:33:02 +0200

    Refactored hello-world Python project to learn playing the SEGA game Sonic the Hedgehog.

---
+++ tests/unit-tests/tests/demo/ai_learns_playing_sonic/ai_learns_playing_sonic_test.py
@@ -0,0 +1,9 @@
+from demo.ai_learns_playing_sonic.ai_learns_playing_sonic import main, ai_learns_playing_sonic
+
+
+def test_main():
+    assert main() == 'Hello Sonic, here I come!'
+
+
+def test_name():
+    assert ai_learns_playing_sonic() is None
--- tests/unit-tests/tests/demo/hello_world/hello_world_test.py
+++
@@ -1,9 +0,0 @@
-from demo.ai_learns_playing_sonic.ai_learns_playing_sonic import main, print_main
-
-
-def test_main():
-    assert main() == 'Hello world!'
-
-
-def test_name():
-    assert print_main() is None
