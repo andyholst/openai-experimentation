3fb5e16c5af73a0a3a3a9882a1229f7414abb5eb
Andy Holst <andy.holst@protonmail.com>
2022-05-23 01:44:04 +0200

    Updated CI pipeline to execute proper SONIC test (#6)
    Updated CI pipeline to fetch private submodule with ownership of the
    roms to be able to execute real SONIC reinforced learning tests.
    
    Updated the Sonic main code to be able to assert the result.
    
    Added a unit test to verify the AI learn to plays the SONIC game with
    the given reward boundaries.

--- src/demo/ai_learns_playing_sonic/ai_learns_playing_sonic.py
+++ src/demo/ai_learns_playing_sonic/ai_learns_playing_sonic.py
@@ -9,13 +9,31 @@ def main():
     about_to_play_sonic()
     env = retro.make(game="SonicTheHedgehog-Genesis", state="GreenHillZone.Act1")
     env.reset()
+    time = 0
+    total_reward = 0
     while True:
-        obs, rew, done, info = env.step(env.action_space.sample())
-        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        time += 1
+        if time % 10 == 0:
+            if info:
+                info_content = {key: value for key, value in info.items()}
+                print(f'time={time}, info: {info_content}')
+            env.render()
+        total_reward += reward
+        if reward > 0:
+            print(f'time: {time}, reward: {reward}, current_reward: {total_reward}')
+        if reward < 0:
+            print(f'time: {time}, penalty: {reward}, current_reward: {total_reward}')
         if done:
-            env.reset()
-    env.close()
+            # This happens both, when time and lives are up, and when game is completed
+            env.render()
+            print('done!')
+            break
+    print(f'time: {time}, total_reward: {total_reward}')
+    return {'time': time, 'total_reward': total_reward}
 
 
 if __name__ == "__main__":
     print(about_to_play_sonic())
+    main()
