c7463cfdd6fdab007a998670580157de3659ed89
Andy Holst <andy.holst@protonmail.com>
2022-06-18 01:54:20 +0200

    ...

--- docker-files/openai-environment/verify_pytorch_is_installed.py
+++ docker-files/openai-environment/verify_pytorch_is_installed.py
@@ -5,8 +5,12 @@ import torch
 ROCM_ARCH = os.getenv('ROCM_ARCH')
 
 if not torch.__version__:
-    raise Exception('PyTorch not currently installed.')
+    raise SystemError('PyTorch not successfully installed.')
 
 if ROCM_ARCH:
     if not torch.cuda.is_available():
-        raise Exception('ROCm feature not correctly installed with PyTorch')
+        raise SystemError('ROCm feature not correctly installed with PyTorch.')
+
+    print('Number of CUDA devices are: ' + str(torch.cuda.device_count()))
+    for i in range(torch.cuda.device_count()):
+        print('CUDA device: ' + torch.cuda.get_device_name(i))
