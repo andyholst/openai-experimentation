e7097215c69c94f36ed8870985a1b3b972caf485
Andy Holst <andy.holst@protonmail.com>
2022-07-10 14:40:47 +0200

    Refactored the PyTorch packaging process
    Refactored the PyTorch packaging logic to include ROCm version in the
    wheel file to know what ROCm cmake build version was used for
    compiling PyTorch. This makes it easier to select proper HOST ROCm GPU
    computing platform version.

--- docker-files/amd-pytorch-release/build_and_release_pytorch_package_from_source.sh
+++ docker-files/amd-pytorch-release/build_and_release_pytorch_package_from_source.sh
@@ -8,6 +8,12 @@ if [ ! -z "$ROCM_ARCH" ]; then
   cd pytorch || exit 1
   python3.7 tools/amd_build/build_amd.py || exit 1
 
+  PYTORCH_BUILD_VERSION="$(python3.7 -c 'from tools.generate_torch_version import get_torch_version; print(get_torch_version())')-rocm-" || exit 1
+  PYTORCH_BUILD_NUMBER="1"
+  PYTORCH_BUILD_VERSION="$PYTORCH_BUILD_VERSION$(echo "$ROCM_VERSION" | tr . 0)"
+
+  export PYTORCH_BUILD_NUMBER PYTORCH_BUILD_VERSION
+
   USE_ROCM=1 PYTORCH_ROCM_ARCH="$ROCM_ARCH" python3.7 setup.py bdist_wheel
   python3.7 -m twine upload -r local dist/* || exit 1
 fi
