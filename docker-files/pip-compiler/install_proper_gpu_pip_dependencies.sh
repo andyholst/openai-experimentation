31e7fbb150767920a24ef1cd1f0bb009a0bf280e
Andy Holst <andy.holst@protonmail.com>
2022-07-10 04:50:32 +0200

    Refactored OpenAI Dockerfile to build proper torch (#11)
    Refactored the OpenAI Dockerfile to install the nessesary dependecies
    for PyTorch if the architecture is AMD ROCm device.
    
    Refactored the pip compiler setup to install proper PyTorch wheel
    file if GPU architecture is ROCm, CUDA or regular CPU.

--- docker-files/pip-compiler/install_proper_gpu_pip_dependencies.sh
+++ docker-files/pip-compiler/install_proper_gpu_pip_dependencies.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 
-lspci | grep -i amd
-
-HAS_AMD_CARD=$?
-
-lspci | grep -i nvidia
-
-HAS_NVIDIA_CARD=$?
-
 EXTRA_INDEX_URL=""
-ROCM_ARCH=""
 IN_REQUIREMENTS=$1
 
-if [ $HAS_AMD_CARD -eq 0 ]; then
-  rocminfo | grep gfx
-  if [ $? -eq 0 ]; then
-    ROCM_ARCH=$(rocminfo | grep -m1 gfx | awk '{print $2}')
+if [ ! -z $ROCM_ARCH ]; then
     if [ $ROCM_ARCH == "gfx803" ]; then
       sed -i '/torch.*/d' "${IN_REQUIREMENTS}"
-      echo "git+https://github.com/ROCmSoftwarePlatform/pytorch@master#egg=torch" >> "${IN_REQUIREMENTS}"
+      echo 'torch' >> "${IN_REQUIREMENTS}"
+      EXTRA_INDEX_URL="--pre --extra-index-url https://docondee.jfrog.io/artifactory/api/pypi/docondee-rocm-pypi/simple"
     else
       EXTRA_INDEX_URL="--pre --extra-index-url https://download.pytorch.org/whl/nightly/rocm5.1.1/"
     fi
-  fi
-elif [ $HAS_NVIDIA_CARD -eq 0 ]; then
+elif [ ! -z $CUDA_ARCH ]; then
   CUDA_VERSION="$(nvcc --version | grep "release" | awk '{print $6}' | cut -c2-)"
   if [ $CUDA_VERSION >= "11.3" ]; then
     EXTRA_INDEX_URL="--pre --extra-index-url https://download.pytorch.org/whl/cu113"
@@ -32,9 +20,7 @@ elif [ $HAS_NVIDIA_CARD -eq 0 ]; then
   fi
 fi
 
-echo "EXTRA_INDEX_URL is $EXTRA_INDEX_URL"
-
-if [ -z "$EXTRA_INDEX_URL" ] && [ -z "$ROCM_ARCH" ]; then
+if [ -z "$EXTRA_INDEX_URL" ]; then
     EXTRA_INDEX_URL="--extra-index-url https://download.pytorch.org/whl/cpu"
 fi
 
--- get_rocm_arch.sh
+++ get_rocm_arch.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-rocminfo | grep -m1 gfx | awk '{print $2}'
+ls /opt/rocm/bin/rocminfo > /dev/null
+
+if [ $? -eq 0 ]; then
+  ROCM_ARCH=$(rocminfo | grep -m1 gfx | awk '{print $2}')
+  echo $ROCM_ARCH
+else
+  echo ""
+fi
+
