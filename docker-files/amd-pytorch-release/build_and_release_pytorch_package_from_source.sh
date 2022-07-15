06ee0bffea68375b20b171f47000daacddd000c4
Andy Holst <andy.holst@protonmail.com>
2022-07-15 23:35:21 +0200

    Reconfigured build & deploy pipeline of PyTorch (#14)
    Reconfigured build and deploy pipeline for PyTorch with the
    flexibility to build and deploy PyTorch for specific ROCm
    architectures and ROCm version.

--- docker-files/amd-pytorch-release/build_and_release_pytorch_package_from_source.sh
+++ docker-files/amd-pytorch-release/build_and_release_pytorch_package_from_source.sh
@@ -11,9 +11,10 @@ if [ ! -z "$ROCM_ARCH" ]; then
   PYTORCH_BUILD_VERSION="$(python3.7 -c 'from tools.generate_torch_version import get_torch_version; print(get_torch_version())')-rocm-" || exit 1
   PYTORCH_BUILD_NUMBER="1"
   PYTORCH_BUILD_VERSION="$PYTORCH_BUILD_VERSION$(echo "$ROCM_VERSION" | tr . 0)"
+  PYTORCH_ROCM_ARCH="$ROCM_ARCH"
 
-  export PYTORCH_BUILD_NUMBER PYTORCH_BUILD_VERSION
+  export PYTORCH_BUILD_NUMBER PYTORCH_BUILD_VERSION PYTORCH_ROCM_ARCH
 
-  USE_ROCM=1 PYTORCH_ROCM_ARCH="$ROCM_ARCH" python3.7 setup.py bdist_wheel
+  USE_ROCM=1 python3.7 setup.py bdist_wheel
   python3.7 -m twine upload -r local dist/* || exit 1
 fi
