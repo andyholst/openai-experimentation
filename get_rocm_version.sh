db9bded552c3907890aec2268f9996e0620039a5
Andy Holst <andy.holst@protonmail.com>
2022-07-16 17:34:28 +0200

    ...

--- get_rocm_version.sh
+++ get_rocm_version.sh
@@ -5,7 +5,6 @@ ls /opt/rocm/lib/librocm-core.so.* > /dev/null
 if [ $? -eq 0 ]; then
   ROCM_VERSION=$(ls /opt/rocm/lib/librocm-core.so.* | sed -r 's/.*librocm-core\.so\.1\.0\.([0-9]+)/\1/')
   ROCM_VERSION="$(echo $ROCM_VERSION | tr 0 .)"
-  echo "ROCm version is $ROCM_VERSION"
   regexp='^([1-9]+\.[0-9]+)(\.\.)$'
   if [[ $ROCM_VERSION =~ $regexp ]]; then
     ROCM_VERSION=$(echo $ROCM_VERSION | sed -E "s/$regexp/\1.0/g")
