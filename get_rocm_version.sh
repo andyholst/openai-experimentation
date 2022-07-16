f232569164d70823b534e788f4b802512fb0cc7d
Andy Holst <andy.holst@protonmail.com>
2022-07-16 17:18:47 +0200

    Updated get ROCm bash script
    Updated get ROCm bash script to get ROCm version correctly based on
    major and minor version and where patch version is 0 aka. no
    patches.

--- get_rocm_version.sh
+++ get_rocm_version.sh
@@ -4,7 +4,13 @@ ls /opt/rocm/lib/librocm-core.so.* > /dev/null
 
 if [ $? -eq 0 ]; then
   ROCM_VERSION=$(ls /opt/rocm/lib/librocm-core.so.* | sed -r 's/.*librocm-core\.so\.1\.0\.([0-9]+)/\1/')
-  echo $ROCM_VERSION | tr 0 .
+  ROCM_VERSION="$(echo $ROCM_VERSION | tr 0 .)"
+  echo "ROCm version is $ROCM_VERSION"
+  regexp='^([1-9]+\.[0-9]+)(\.\.)$'
+  if [[ $ROCM_VERSION =~ $regexp ]]; then
+    ROCM_VERSION=$(echo $ROCM_VERSION | sed -E "s/$regexp/\1.0/g")
+  fi
+  echo $ROCM_VERSION
 else
   echo ""
 fi
