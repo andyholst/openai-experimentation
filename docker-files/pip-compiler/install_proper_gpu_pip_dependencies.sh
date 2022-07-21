1415cf47eaf60cfcc6423b123ae29abf9e759b18
Andy Holst <andy.holst@protonmail.com>
2022-07-21 17:35:21 +0200

    Refactor ROCm architecture variable and PyTorch CI pipeline (#18)
    Updated ROCM_ARCH variable to include gfx models 803, 900, 906 and
    908 when building the PyTorch Python wheel file with ROC;_VERSION
    5.2 as default version.
    
    Updated ROCM_ARCH variable get value with consistent "${}" variable
    value to deal with ambiguity in expressions and to include with spaces
    as single word.
    
    * Updated bash script to install proper GPU Python PYPI Torch wheel
    dependencies from one PYPI source since the ROCm architectures
    by default includes the gfx models 803, 900, 906 and 908.

--- docker-files/pip-compiler/install_proper_gpu_pip_dependencies.sh
+++ docker-files/pip-compiler/install_proper_gpu_pip_dependencies.sh
@@ -3,17 +3,13 @@
 EXTRA_INDEX_URL=""
 IN_REQUIREMENTS=$1
 
-if [ ! -z $ROCM_ARCH ]; then
-    if [ $ROCM_ARCH == "gfx803" ]; then
-      sed -i '/torch.*/d' "${IN_REQUIREMENTS}"
-      uri="https://docondee.jfrog.io/artifactory/api/pypi/docondee-rocm-pypi/simple"
-      TORCH_VERSION=$(curl "$uri"/torch/ | sed 's/<\/*[^>]*>//g' | grep torch | sed 's/-cp.*//g' | grep -i "$ROCM_VERSION" | head -1)
-      TORCH_PACKAGE_VERSION=$(echo $TORCH_VERSION | sed 's/torch-/torch==/g')
-      echo "${TORCH_PACKAGE_VERSION}" >> "${IN_REQUIREMENTS}"
-      EXTRA_INDEX_URL="--pre --extra-index-url $uri"
-    else
-      EXTRA_INDEX_URL="--pre --extra-index-url https://download.pytorch.org/whl/nightly/rocm5.1.1/"
-    fi
+if [ ! -z "${ROCM_ARCH}" ]; then
+    sed -i '/torch.*/d' "${IN_REQUIREMENTS}"
+    uri="https://docondee.jfrog.io/artifactory/api/pypi/docondee-rocm-pypi/simple"
+    TORCH_VERSION=$(curl "$uri"/torch/ | sed 's/<\/*[^>]*>//g' | grep torch | sed 's/-cp.*//g' | grep -i "$ROCM_VERSION" | head -1)
+    TORCH_PACKAGE_VERSION=$(echo $TORCH_VERSION | sed 's/torch-/torch==/g')
+    echo "${TORCH_PACKAGE_VERSION}" >> "${IN_REQUIREMENTS}"
+    EXTRA_INDEX_URL="--pre --extra-index-url $uri"
 elif [ ! -z $CUDA_ARCH ]; then
   CUDA_VERSION="$(nvcc --version | grep "release" | awk '{print $6}' | cut -c2-)"
   if [ $CUDA_VERSION >= "11.3" ]; then
