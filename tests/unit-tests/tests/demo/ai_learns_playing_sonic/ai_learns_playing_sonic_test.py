06c7d8b91848504e4127c8665fe49f2a4c0ed2b9
Andy Holst <andy.holst@protonmail.com>
2022-07-23 19:34:21 +0200

    Created a new trained Sonic agent pipeline
    Created a new train Sonic agent pipeline based on a specific Sonic
    game and state aka. level and to upload the trained agent to the
    artifactory containing trained agents for specific game and level.
    
    Refined the CI pipeline to only execute Sonic the Hedgehog tests with
    trained skills for a specific environment.

--- tests/unit-tests/tests/demo/ai_learns_playing_sonic/ai_learns_playing_sonic_test.py
+++ tests/unit-tests/tests/demo/ai_learns_playing_sonic/ai_learns_playing_sonic_test.py
@@ -1,14 +1,23 @@
+import os
+
+import pytest
 import retro
+
+from datetime import datetime
+from os.path import exists
+
 from demo.ai_learns_playing_sonic.ai_learns_playing_sonic import about_to_play_sonic, main
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 
+@pytest.mark.test_sonic_skills
 def test_about_to_play_sonic():
     # When & then
     assert about_to_play_sonic() == 'You rock Sonic!'
 
 
+@pytest.mark.test_sonic_skills
 def test_ai_to_play_the_sonic_game():
     # Given
     environment = retro.make(game="SonicTheHedgehog-Genesis", state="GreenHillZone.Act1")
@@ -22,13 +31,35 @@ def test_ai_to_play_the_sonic_game():
     assert 100 < dictionary_result['total_reward'] <= 300
 
 
-def test_train_ai_to_be_better_at_playing_the_sonic_game():
+@pytest.mark.train_sonic_agent
+def test_train_ai_to_be_better_at_playing_the_sonic_game(game=os.getenv('SONIC_GAME'), state=os.getenv('SONIC_STATE')):
     # Given
-    environment = DummyVecEnv([lambda: retro.make(game="SonicTheHedgehog-Genesis", state="GreenHillZone.Act1")])
+    assert game
+    assert state
+
+    environment = DummyVecEnv([lambda: retro.make(game=game, state=state)])
     environment = VecNormalize(environment, norm_obs=True, norm_reward=False, clip_obs=10.)
 
     agent = PPO(policy='MlpPolicy', env=environment, verbose=1)
+
+    # When
     agent.learn(total_timesteps=1000)
+
+    # Then
+    filename = f'sonic_agent_for_{game}_on_state_{state}_{datetime.now().strftime("%Y-%m-%dT%H_%M_%SZ")}.agent'
+    agent.save(filename)
+
+    assert exists(filename)
+
+
+@pytest.mark.test_sonic_agent
+def test_sonic_agent(game='SonicTheHedgehog-Genesis', state='GreenHillZone.Act1', agent=None):
+    # Given
+    assert agent
+
+    environment = DummyVecEnv([lambda: retro.make(game=game, state=state)])
+    environment = VecNormalize(environment, norm_obs=True, norm_reward=False, clip_obs=10.)
+    agent = PPO.load(path=agent, env=environment, verbose=1)
 
     # When
     dictionary_result = main(environment, agent)
