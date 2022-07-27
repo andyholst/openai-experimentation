cccf911cbd8e99a461a980ae6d65be0c73f0d688
Andy Holst <andy.holst@protonmail.com>
2022-07-27 04:00:43 +0200

    ...

--- setup_agent_filename_and_filepath_env.sh
+++ setup_agent_filename_and_filepath_env.sh
@@ -3,7 +3,4 @@
 SONIC_AGENT_NAME="$(find tests/unit-tests -name "*.agent" | xargs -n 1 basename)"
 SONIC_AGENT_FILE="$(find tests/unit-tests -name "*.agent" | xargs -n 1)"
 
-echo "SONIC_AGENT_NAME is $SONIC_AGENT_NAME"
-echo "SONIC_AGENT_FILE is $SONIC_AGENT_FILE"
-
 export SONIC_AGENT_NAME SONIC_AGENT_FILE
