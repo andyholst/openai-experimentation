6a66fb0adc8e897bf8efda752cb68b73d616021c
Andy Holst <andy.holst@protonmail.com>
2022-07-31 19:01:39 +0200

    Refactored the AMD ROCm PyTorch pipeline
    Refactored the AMD ROCm PyTorh pipeline to upload the ROCm PyTorch
    wheel file to GitHub openai-experimentation repository by default.
    
    Added an UPLOAD_TO_ARTIFACTORY parameter to be able to upload the
    ROCm PyTorch wheel file to the PYPI artifactory.

--- docker-files/amd-pytorch-release/build_and_release_pytorch_package_from_source.sh
+++ docker-files/amd-pytorch-release/build_and_release_pytorch_package_from_source.sh
@@ -5,7 +5,7 @@ if [ ! -z "${ROCM_ARCH}" ]; then
   export MAX_JOBS=$(nproc)
   export NINJAFLAGS="-j$(nproc)"
 
-  cd pytorch || exit 1
+  cd /app/src/build/pytorch || exit 1
   python3.7 tools/amd_build/build_amd.py || exit 1
 
   PYTORCH_BUILD_VERSION="$(python3.7 -c 'from tools.generate_torch_version import get_torch_version; print(get_torch_version())')-rocm-" || exit 1
@@ -16,5 +16,8 @@ if [ ! -z "${ROCM_ARCH}" ]; then
   export PYTORCH_BUILD_NUMBER PYTORCH_BUILD_VERSION PYTORCH_ROCM_ARCH
 
   USE_ROCM=1 python3.7 setup.py bdist_wheel
-  python3.7 -m twine upload -r local dist/* || exit 1
+
+  if [ "${UPLOAD_TO_ARTIFACTORY}" == "True" ]; then
+    python3.7 -m twine upload -r local dist/* || exit 1
+  fi
 fi
