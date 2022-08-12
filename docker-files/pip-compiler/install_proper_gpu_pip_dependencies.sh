ada64bdc41d9e3326231975df21525325acd44f6
Andy Holst <andy.holst@protonmail.com>
2022-08-12 03:40:44 +0200

    Refactored the installment of the PyTorch PYPI dependency (#29)
    Refactored the PYPI PyTorch dependency installment depending on
    if the uniform resource identifier is github or artifactory.

--- docker-files/pip-compiler/install_proper_gpu_pip_dependencies.sh
+++ docker-files/pip-compiler/install_proper_gpu_pip_dependencies.sh
@@ -5,13 +5,20 @@ IN_REQUIREMENTS=$1
 
 if [ ! -z "${ROCM_ARCH}" ]; then
     sed -i '/torch.*/d' "${IN_REQUIREMENTS}"
+    if [ "${PYPI_PLACE}" == "artifactory" ]; then
     uri="https://${PYPI_USERNAME}:${PYPI_PASSWORD}@docondee.jfrog.io/artifactory/api/pypi/docondee-rocm-pypi/simple"
     TORCH_VERSION=$(curl "$uri"/torch/ | sed 's/<\/*[^>]*>//g' | grep torch | sed 's/-cp.*//g' | grep -i "$ROCM_VERSION" | head -1)
     TORCH_PACKAGE_VERSION=$(echo $TORCH_VERSION | sed 's/torch-/torch==/g')
     echo "${TORCH_PACKAGE_VERSION}" >> "${IN_REQUIREMENTS}"
     EXTRA_INDEX_URL="--pre --extra-index-url $uri"
-elif [ ! -z $CUDA_ARCH ]; then
-  CUDA_VERSION="$(nvcc --version | grep "release" | awk '{print $6}' | cut -c2-)"
+    else
+      uri="https://github.com/andyholst/openai-experimentation/releases"
+      torch_file=$(curl $uri | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep -i releases/download/torch | grep -i "$ROCM_VERSION" | head -1)
+      uri="https://github.com"
+      echo "${uri}${torch_file}" >> "${IN_REQUIREMENTS}"
+    fi
+elif [ ! -z ${CUDA_ARCH} ]; then
+  CUDA_VERSION="$(nvcc --version | grep "release" | awk '{print $6}' | cut -c2- || exit 1)"
   if [ $CUDA_VERSION >= "11.3" ]; then
     EXTRA_INDEX_URL="--pre --extra-index-url https://download.pytorch.org/whl/cu113"
   else
@@ -20,7 +27,15 @@ elif [ ! -z $CUDA_ARCH ]; then
 fi
 
 if [ -z "$EXTRA_INDEX_URL" ]; then
-    EXTRA_INDEX_URL="--extra-index-url https://download.pytorch.org/whl/cpu"
+    if [ "${PYPI_PLACE}" == "artifactory" ]; then
+      EXTRA_INDEX_URL="--extra-index-url https://download.pytorch.org/whl/cpu"
+    else
+      sed -i '/torch.*/d' "${IN_REQUIREMENTS}"
+      uri="https://github.com/andyholst/openai-experimentation/releases"
+      torch_file=$(curl $uri | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep -i releases/download/torch | grep -i "cpu" | head -1)
+      uri="https://github.com"
+      echo "${uri}${torch_file}" >> "${IN_REQUIREMENTS}"
+    fi
 fi
 
 export EXTRA_INDEX_URL
