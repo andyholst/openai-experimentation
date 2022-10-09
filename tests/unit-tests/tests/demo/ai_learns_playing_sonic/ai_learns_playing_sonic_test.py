71946bbf83f6f506552170d42396bfe5c9c39599
Andy Holst <andy.holst@protonmail.com>
2022-10-09 18:45:58 +0200

    Fixed tensorboard log (#31)
    Fixed tensorboard logs and improved trained agent asserts.

--- tests/unit-tests/tests/demo/ai_learns_playing_sonic/ai_learns_playing_sonic_test.py
+++ tests/unit-tests/tests/demo/ai_learns_playing_sonic/ai_learns_playing_sonic_test.py
@@ -4,16 +4,17 @@ import platform
 import pytest
 import re
 import retro
+import shutil
 import tempfile
 import urllib.request
 import validators
 
 from datetime import datetime
-from os.path import exists
+from os.path import exists, isfile
 from pathlib import Path
 
-from tests.demo.ai_learns_playing_sonic.train_log_callback import TrainLogCallback
 from demo.ai_learns_playing_sonic.ai_learns_playing_sonic import about_to_play_sonic, main
+from tests.demo.ai_learns_playing_sonic.train_log_callback import TrainLogCallback
 
 from stable_baselines3 import (
     A2C,
@@ -75,7 +76,7 @@ def test_train_ai_to_be_better_at_playing_the_sonic_game(game=os.getenv('SONIC_G
     filename = filename.replace('.', '_')
 
     checkpoint_dir = os.getenv('CHECKPOINT_DIR', './model/train/')
-    log_dir = os.getenv('LOG_DIR', '/.model/logs/')
+    log_dir = os.getenv('LOG_DIR', './model/logs/')
 
     agent_callback = TrainLogCallback(check_freq=10000, save_path=checkpoint_dir, filename=filename)
 
@@ -98,7 +99,13 @@ def test_train_ai_to_be_better_at_playing_the_sonic_game(game=os.getenv('SONIC_G
     filename = f'{filename}.agent'
 
     agent.save(filename)
+    shutil.copyfile(filename, f'{checkpoint_dir}{filename}')
+
     assert exists(filename)
+    assert isfile(filename)
+
+    assert exists(f'{checkpoint_dir}{filename}')
+    assert isfile(f'{checkpoint_dir}{filename}')
 
 
 @pytest.mark.test_sonic_agent
